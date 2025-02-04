342dee90712cf29133989768df1360353ed1983f
Daisywf <wf.daisy@gmail.com>
2025-02-04 17:30:27 +0800

    Create index.vue

---
+++ src/pages/mbti/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="mbti-container">
+    <view class="title">MBTI 性格测试</view>
+    <!-- 待完善具体内容 -->
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 待添加数据
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.mbti-container {
+  padding: 20rpx;
+  
+  .title {
+    font-size: 32rpx;
+    font-weight: bold;
+    margin-bottom: 30rpx;
+  }
+}
+</style>
