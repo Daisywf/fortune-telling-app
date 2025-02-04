e6c53ba0d2cd95679ea6dd66564afa781d2623be
Daisywf <wf.daisy@gmail.com>
2025-02-04 17:30:55 +0800

    Create index.vue

---
+++ src/pages/fortune/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="fortune-container">
+    <view class="title">命理分析</view>
+    <!-- 待完善具体内容 -->
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 待添加数据
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.fortune-container {
+  padding: 20rpx;
+  
+  .title {
+    font-size: 32rpx;
+    font-weight: bold;
+    margin-bottom: 30rpx;
+  }
+}
+</style>
