a355b7b2449e1ff5786c1fad2b7d567c7ff16b75
Daisywf <wf.daisy@gmail.com>
2025-02-04 18:07:24 +0800

    Update index.vue

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,7 +4,7 @@
     <view class="logo-area">
       <image 
         class="logo" 
-        src="https://drive.google.com/file/d/1UJp9ZITPGg4o8duCqbqCr3RaV_LuZegr/view?usp=sharing" 
+        src="https://ibb.co/jvZwmm6p" 
         mode="aspectFit"
       ></image>
       <view class="slogan">Scientific Astrology for Modern Life</view>
